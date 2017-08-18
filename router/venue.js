eb7eb1ed6b1a10e8b56c283a649669015cec344b
tamastro <rpratama45@gmail.com>
2017-08-18 10:25:11 +0700

    UD venue

--- router/venue.js
+++ router/venue.js
@@ -4,11 +4,11 @@ const db = require('../models');
 
 router.get('/', (req, res) => {
   db.Venue.findAll()
-  .then(ven => {
-    res.render('venue', {
-      data: ven
+    .then(ven => {
+      res.render('venue', {
+        data: ven
+      })
     })
-  })
 })
 
 router.post('/', (req, res) => {
@@ -16,44 +16,93 @@ router.post('/', (req, res) => {
   res.redirect(`/venue/${req.body.id}`)
 })
 
-router.get('/:idv', (req,res) => {
+router.get('/:idv', (req, res) => {
   thisID = parseInt(req.params.idv)
   db.Venue.findOne({
-    where: {
-      id: thisID
-    }
-  })
-  .then(venue => {
-    db.UserVenue.findAndCountAll({
-      where:{
-        date: req.session.user.tgl,
-        VenueId: req.params.idv,
-        active: true
-      },
-      include: [db.User]
+      where: {
+        id: thisID
+      }
     })
-    .then(result => {
-      res.render('venuedetail', {
-        data: venue,
-        count: result.count,
-        result: result.rows,
-        role: req.session.user.role
-      })
+    .then(venue => {
+      db.UserVenue.findAndCountAll({
+          where: {
+            date: req.session.user.tgl,
+            VenueId: req.params.idv,
+            active: true
+          },
+          include: [db.User]
+        })
+        .then(result => {
+          res.render('venuedetail', {
+            data: venue,
+            count: result.count,
+            result: result.rows,
+            role: req.session.user.role
+          })
+        })
     })
-  })
 })
 
 router.post('/:idv', (req, res) => {
   let iniId = parseInt(req.params.idv)
   db.UserVenue.create({
-    VenueId: iniId,
-    UserId: req.session.user.id,
-    active: req.body.active,
-    date: `${req.session.user.tgl}`
+      VenueId: iniId,
+      UserId: req.session.user.id,
+      active: req.body.active,
+      date: `${req.session.user.tgl}`
+    })
+    .then(() => {
+      res.redirect(`/venue/${iniId}`)
+    })
+})
+
+router.get('/edit/:idv', (req, res) => {
+  db.Venue.findOne({
+    where: {
+      id: req.params.idv
+    }
   })
-  .then(() => {
-    res.redirect(`/venue/${iniId}`)
-  })
+    .then(ven => {
+      res.render('editV', {
+        edit: ven,
+      })
+    })
+})
+
+router.post('/edit/:idv', (req, res) => {
+  db.Venue.update({
+    name: `${req.body.name}`,
+    quota: `${req.body.quota}`,
+    photo: `${req.body.photo}`,
+    price: `${req.body.price}`,
+    address: `${req.body.alamat}`,
+    updatedAt: new Date()
+  }, {
+      where: {
+        id: req.params.idv
+      }
+    })
+    .then(() => {
+      res.redirect(`/venue/${req.params.idv}`)
+    })
+})
+
+router.get('/delete/:idv', (req, res) => {
+  db.UserVenue.destroy({
+      where: {
+        VenueId: req.params.idv
+      }
+    })
+    .then(() => {
+      db.Venue.destroy({
+          where: {
+            id: req.params.idv
+          }
+        })
+        .then(() => { 
+          res.redirect('/venue')
+        })
+    })
 })
 
 router.use((req, res, next) => {
@@ -72,15 +121,17 @@ router.get('/add/venue', (req, res) => {
 
 router.post('/add/venue', (req, res) => {
   db.Venue.create({
-    name: `${req.body.name}`,
-    quota: `${req.body.quota}`,
-    photo: `${req.body.photo}`,
-    price: `${req.body.price}`,
-    address: `${req.body.alamat}`
-  })
-  .then(() => {
-    res.redirect('/user/profile')
-  })
+      name: `${req.body.name}`,
+      quota: `${req.body.quota}`,
+      photo: `${req.body.photo}`,
+      price: `${req.body.price}`,
+      address: `${req.body.alamat}`,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    })
+    .then(() => {
+      res.redirect('/user/profile')
+    })
 })
 
 module.exports = router
